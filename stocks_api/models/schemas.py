1a52ec813bcc1c7ae4d38d72d4f6422493cdc192
Stephen <stephendeanharper@gmail.com>
2018-09-05 22:26:35 -0700

    added in docstrings to schemas.py

--- stocks_api/models/schemas.py
+++ stocks_api/models/schemas.py
@@ -4,11 +4,18 @@ from . import Portfolio, Stock, Account, AccountRole
 
 
 class AccountRoleSchema(ModelSchema):
+    """this schema model is based on the account role. The meta class references
+    the meta model and will be filled with the Role model.
+    """
     class Meta:
         model = AccountRole
 
 
 class AccountSchema(ModelSchema):
+    """this schema model is based on the account. The meta class references
+    the meta model and will be filled with the Account model. the roles
+    field will also use the role schema from above and use the name
+    """
     roles = fields.Nested(AccountRoleSchema, many=True, only='name')
 
     class Meta:
@@ -16,6 +23,10 @@ class AccountSchema(ModelSchema):
 
 
 class PortfolioSchema(ModelSchema):
+    """this schema model is based on the portfolio. this will build upon the
+    two above schemas, the roles and accounts. the account section will not
+    include the password, locations, roles and dates for security reasons.
+    """
     roles = fields.Nested(AccountRoleSchema, many=True, only='name')
     account = fields.Nested(AccountSchema, exclude=(
         'password', 'locations', 'roles', 'date_created', 'date_updated',
@@ -26,6 +37,11 @@ class PortfolioSchema(ModelSchema):
 
 
 class StockSchema(ModelSchema):
+    """this schema model is based on the stock. this will also build on
+    the account and role schemas. similarly true as the portfolio schema,
+    the account section will not include the password, locations, roles,
+    and dates for security reasons
+    """
     roles = fields.Nested(AccountRoleSchema, many=True, only='name')
     account = fields.Nested(AccountSchema, exclude=(
         'password', 'locations', 'roles', 'date_created', 'date_updated',
